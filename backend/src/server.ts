add7e269ac5554620ac577eb537a3f430a651062
Alisultanov Shamil <Shamil.Alisultanov@transitcard.ru>
2026-02-09 14:54:29 +0300

    feat: update

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const https = require('https');
 import http from 'http';
 
 import app from './app';
@@ -11,6 +13,17 @@ const wsService = new WebSocketService(server);
 
 // Экспортируем wsService для использования в контроллерах
 export { wsService };
+
+const options = {
+  key: fs.readFileSync('/root/155.212.218.104-key.pem'),
+  cert: fs.readFileSync('/root/155.212.218.104.pem')
+};
+
+// Запуск HTTPS сервера
+https.createServer(options, app).listen(443, () => {
+  console.log('HTTPS сервер запущен на порту 443');
+  console.log('Откройте в браузере: https://123.123.123.123');
+});
 
 server.listen(PORT, () => {
   console.log(`Сервер запущен на порту ${PORT}`);
